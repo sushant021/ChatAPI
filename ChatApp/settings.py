0838dddd73d54ed454100737e10d5297143834bd
sushant <nsushant021@gmail.com>
2025-07-02 17:27:30 -0600

    Added CORS settings

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -23,9 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wbugsh$g=yde8lrl&w(nhwb65&b(3*1n$5y6r75#-mv(2rs3)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    
+    '.onrender.com',              # Allow all Render subdomains
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -38,10 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'chatapi'
+    'chatapi',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'ChatApp.urls'
 
